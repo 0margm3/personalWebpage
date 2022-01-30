3318b37f36c45e04c3cacd6e749728d12bbf7951
in1x <omar.gerardo@tup.edu.ph>
2022-01-30 20:03:55 +0800

    Create Home

---
+++ Home/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '600px',
+      'md': '812px',
+      'lg': '1100px',
+    },
+  },
+  plugins: [],
+}
