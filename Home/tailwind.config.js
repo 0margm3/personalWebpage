5fd8624418b80e331e4bc2545c5fe31e21bf5e66
in1x <omar.gerardo@tup.edu.ph>
2022-01-31 01:23:06 +0800

    ..

--- Home/tailwind.config.js
+++ Home/tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["*.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      'EncodeSansCondensed': ["'Encode Sans Condensed'", "sans-serif"],
+      'Montserrat': ["'Montserrat'", "sans-serif"],
+      'SupermercadoOne': ["'Supermercado One'", "cursive"],
+      'RobotoMono': ["'Roboto Mono'", "monospace"]
+    },
     screens: {
       'sm': '600px',
       'md': '812px',
